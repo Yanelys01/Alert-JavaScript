c598fc43ab18a86f27712fbe6ad3bbe0b607cfbd
Yanelys01 <91100469+Yanelys01@users.noreply.github.com>
2021-10-01 18:28:54 -0500

    Add files via upload

---
+++ Register.js
@@ -0,0 +1,38 @@
+// Register Or Login //
+
+const registrousuario = () =>{
+    let Usuario = prompt ("Ingresar credenciales del usuario ").toLowerCase();
+
+    switch(Usuario) {
+        
+        case "kaled":
+            alert("Nombre del " + Usuario+ " es correcto")
+            break;
+            case "yanelys":
+                alert("Nombre del " + Usuario+ " es correcto")
+                break;
+                case "sandra":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "camila":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "catalina":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "karen":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "juliana":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "tito":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "jorman":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "julian":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;  case "santiago":
+                    alert("Nombre del " + Usuario+ " es correcto")
+                    break;
+    
+        default:
+            alert("No Identificado")
+            break;
+    }
+}
